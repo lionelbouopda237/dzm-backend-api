b8b06ec3d64cc5c91bcd0130bd20b03e73ec2281
lionelbouopda237 <bdlionel91@gmail.com>
2026-03-17 22:13:29 +0100

    Delete supabase_v4_migration.sql

--- supabase_v4_migration.sql
+++
@@ -1,8 +0,0 @@
-alter table if exists factures
-  add column if not exists image_url text,
-  add column if not exists image_public_id text;
-
-alter table if exists paiements_mobile
-  add column if not exists image_url text,
-  add column if not exists image_public_id text,
-  add column if not exists operateur text;
